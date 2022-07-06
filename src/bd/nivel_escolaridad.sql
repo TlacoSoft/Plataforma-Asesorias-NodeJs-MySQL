8c5597081b62f53bbdebdff9eccb7c547b94f056
strangergirl8 <104879774+strangergirl8@users.noreply.github.com>
2022-07-05 22:15:59 -0500

    Tabla de nivel de escolaridad

---
+++ src/bd/nivel_escolaridad.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-07-2022 a las 05:03:14
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `asesorias`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `nivel_escolaridad`
+--
+
+CREATE TABLE `nivel_escolaridad` (
+  `idNivel` int(11) NOT NULL,
+  `nivel` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `nivel_escolaridad`
+--
+
+INSERT INTO `nivel_escolaridad` (`idNivel`, `nivel`) VALUES
+(1, 'posgrado'),
+(2, 'universidad');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `nivel_escolaridad`
+--
+ALTER TABLE `nivel_escolaridad`
+  ADD PRIMARY KEY (`idNivel`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `nivel_escolaridad`
+--
+ALTER TABLE `nivel_escolaridad`
+  MODIFY `idNivel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
