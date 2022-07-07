4728931e797a6296e4e74ceeb6feef9c6b07efc5
Jezreel777 <oseasquero@gmail.com>
2022-07-07 09:09:18 -0500

    post get delete

--- src/routes/nivel_escolaridad.js
+++ src/routes/nivel_escolaridad.js
@@ -14,4 +14,47 @@ router.get('/get', (req, res) => {
     });
 });
 
+
+
+
+// // DELETE An escolaridad
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('DELETE FROM nivel_escolaridad  WHERE idNivel = ?', [id], (err, rows, fields) => {
+        if (!err) {
+            res.json({ status: 'nivel Deleted' });
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+// // INSERT An User
+router.post('/create', (req, res) => {
+    const { nivel } = req.body;
+    // id must be 0 to create new row
+    const query = 'INSERT INTO `nivel_escolaridad` (`idNivel`, `nivel`) VALUES (NULL, ?)';
+    mysqlConnection.query(query, [nivel], (err, rows, fields) => {
+        if (!err) {
+            res.json({ status: 'nivel Saved' });
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+// //  Update User
+router.put('/:id', (req, res) => {
+    const { nivel } = req.body;
+    const { id } = req.params;
+    const query = 'UPDATE `nivel_escolaridad` SET `nivel` = ? WHERE `nivel_escolaridad`.`idNivel` = ?';
+    mysqlConnection.query(query, [nivel, id], (err, rows, fields) => {
+        if (!err) {
+            res.json({ status: 'nivel Updated' });
+        } else {
+            console.log(err);
+        }
+    });
+});
+
 module.exports = router;
