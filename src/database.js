ce3fffe2d18505d85646922816824ea94869e825
Braquetes <braquetesss@gmail.com>
2022-07-09 22:35:31 -0500

    Index

--- src/database.js
+++ src/database.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql");
 
 const mysqlConnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'asesorias'
+  // host: 'localhost',
+  // user: 'root',
+  // password: '',
+  // database: 'asesorias'
+
+  host:"asesorias.mysql.database.azure.com",
+  user:"braquetes",
+  password:"Rodo*137946*1902",
+  database:"asesorias_db",
+  port:3306, ssl:{ca:fs.readFileSync("{ca-cert filename}")}
+
   // host: process.env.HOST,
   // user: process.env.MYSQL_USER,
   // password: process.env.PASSWORD,
