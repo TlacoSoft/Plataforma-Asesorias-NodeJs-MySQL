21f1af04a7f52c0a2cc762b36d35c213c1b7eab4
Jezreel777 <oseasquero@gmail.com>
2022-08-12 09:02:37 -0500

    profesores get, delete, put, post

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ app.set('port', process.env.PORT || 3000);
 
 // Middlewares
 app.use(express.json());
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
 });
 
 // Routes
@@ -22,6 +22,7 @@ app.get("/", (req, res) => {
 app.use("/users", require('./routes/usuarios'));
 app.use("/nivel_escolaridad", require('./routes/nivel_escolaridad'));
 app.use("/asesorias", require('./routes/asesorias'));
+app.use("/profesoress", require('./routes/profesoress'));
 
 // Starting the server
 app.listen(app.get('port'), () => {
