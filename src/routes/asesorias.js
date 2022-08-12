c59c692a690d03feea6bac22ae40dc35f1bc1ea8
Rodolfo Braquetes <braquetesss@gmail.com>
2022-08-12 14:04:15 -0500

    error-create

--- src/routes/asesorias.js
+++ src/routes/asesorias.js
@@ -5,7 +5,7 @@ const mysqlConnection = require('../database.js');
 
 // GET all Users
 router.get('/get', (req, res) => {
-    mysqlConnection.query('SELECT * FROM asesoria', (err, rows, fields) => {
+    mysqlConnection.query('SELECT * FROM asesoria INNER JOIN usuarios on usuarios.idUsuario = asesoria.alumno', (err, rows, fields) => {
         if (!err) {
             res.json(rows);
         } else {
@@ -14,13 +14,27 @@ router.get('/get', (req, res) => {
     });
 });
 
-
+// Obtener un usuario
+router.get('/:id', (req, res) => {
+    const { id } = req.params; 
+    mysqlConnection.query('SELECT * FROM asesoria WHERE idAsesoria = ?', [id], (err, rows, fields) => {
+      if (!err) {
+        if(rows!=''){
+          res.json(rows[0]);  
+        }else{                
+          res.json({status: "User not found"})
+        }
+      } else {
+        console.log(err);
+      }
+    });
+  });
 
 
 // // DELETE An escolaridad
 router.delete('/:id', (req, res) => {
     const { id } = req.params;
-    mysqlConnection.query('DELETE FROM asesoria  WHERE idAsesoria = ?', [id], (err, rows, fields) => {
+    mysqlConnection.query('DELETE FROM asesoria WHERE idAsesoria = ?', [id], (err, rows, fields) => {
         if (!err) {
             res.json({ status: 'asesoria Deleted' });
         } else {
@@ -33,7 +47,7 @@ router.delete('/:id', (req, res) => {
 router.post('/create', (req, res) => {
     const { tema, encargado, alumno, fecha, hora, modalidad, estatus } = req.body;
     // id must be 0 to create new row
-    const query = 'INSERT INTO `asesoria` (`idAsesoria`, `tema`, `encargado`, `alumno`, `fecha`, `hora`, `modalidad`, `estatus`) VALUES (NULL, ?,?,?,?,?,?,?)';
+    const query = 'INSERT INTO `asesoria` (`idAsesoria`, `tema`, `encargado`, `alumno`, `fecha`, `hora`, `modalidad`, `estatus`) VALUES (NULL,?,?,?,?,?,?,?)';
     mysqlConnection.query(query, [tema, encargado, alumno, fecha, hora, modalidad, estatus], (err, rows, fields) => {
         if (!err) {
             res.json({ status: 'Asesoria Saved' });
